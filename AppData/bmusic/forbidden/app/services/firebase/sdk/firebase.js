fdccbf6a9002c051ab6faf845e5dd14022732690
Jednaz Lonestamp <nissandiezonencaboagripoz9@gmail.com>
2024-09-26 12:28:17 +0800

    Update firebase.js

--- AppData/bmusic/forbidden/app/services/firebase/sdk/firebase.js
+++ AppData/bmusic/forbidden/app/services/firebase/sdk/firebase.js
@@ -8,7 +8,7 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "",
+    apiKey: "API_KEY",
     authDomain: "blackmusic-dd4d3.firebaseapp.com",
     projectId: "blackmusic-dd4d3",
     storageBucket: "blackmusic-dd4d3.appspot.com",
